aac399bbe36e8741b937dec21504946d81f18c18
SanyaSriv <ssrivas7@ucsc.edu>
2022-11-24 02:42:54 -0800

    Getting a non-pixelated cube now

--- asg4.js
+++ asg4.js
@@ -9,7 +9,6 @@ function main() {
   far = 5;
   camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
   camera.position.z = 2;
-
   scene = new THREE.Scene();
   {
     const color = 0xFFFFFF;
@@ -34,13 +33,31 @@ function main() {
 
 function render(time) {
     time *= 0.001;  // convert time to seconds
-   
+    if (resizeRendererToDisplaySize(renderer)) {
+        const canvas = renderer.domElement;
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateProjectionMatrix();
+    }
+    const canvas = renderer.domElement;
+  camera.aspect = canvas.clientWidth / canvas.clientHeight;
+  camera.updateProjectionMatrix();
     cube.rotation.x = time;
     cube.rotation.y = time;
    
     renderer.render(scene, camera);
-   
     requestAnimationFrame(render);
   }
 
 main();
+
+function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const pixelRatio = window.devicePixelRatio;
+    const width  = canvas.clientWidth  * pixelRatio | 0;
+    const height = canvas.clientHeight * pixelRatio | 0;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
